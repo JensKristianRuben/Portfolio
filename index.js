23e3739a1c7407b90e8dc1c7e51290ca715dd352
JensKristianRuben <Jenskristian79@gmail.com>
2025-03-17 18:09:40 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Happy developing ✨')
+createNavBar()
+
+
+
+function createNavBar() {
+    const navBar = document.createElement('nav');
+
+    const ul = document.createElement('ul');
+
+    const about = document.createElement('li');
+    about.innerHTML = "Om"
+    const examples = document.createElement('li');
+    examples.innerHTML = "Se hvad jeg kan"
+    const contact = document.createElement('li');
+    contact.innerHTML = "Kontakt"
+
+    ul.appendChild(about);
+    ul.appendChild(examples);
+    ul.appendChild(contact);
+
+    navBar.appendChild(ul);
+
+    document.body.appendChild(navBar);
+
+}
