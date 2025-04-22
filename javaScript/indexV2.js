9795745bdf010905daeb2bed1201b88d248ecf09
JensKristianRuben <Jenskristian@gmail.dk>
2025-04-22 14:59:05 +0200

    contact buttons added

--- javaScript/indexV2.js
+++ javaScript/indexV2.js
@@ -2,6 +2,14 @@ document.getElementById('projectBtn').addEventListener('click', () => {
     document.querySelector('.mainContainer').classList.add('swipe-left');
 });
 
-document.getElementById("contactBtn").addEventListener('click', () => {
-    document.querySelector('.mainContainer').classList.add('swipe-right');
-})
+document.getElementById("contactBtn").addEventListener("click", () => {
+    const mainContainer = document.querySelector('.mainContainer');
+    const contactContainer = document.querySelector("#contactMainContainer");
+
+    mainContainer.classList.add('swipe-right');
+
+    setTimeout(() => {
+        contactContainer.style.display = 'flex';
+        contactContainer.classList.add("swipe-in");
+    }, 700);
+});
