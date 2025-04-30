7b028bf642a041fd79e626535880f75b8ba3f964
JensKristianRuben <Jenskristian@gmail.dk>
2025-04-30 11:42:51 +0200

    routing added

--- javaScript/indexV2.js
+++ javaScript/indexV2.js
@@ -1,56 +1,86 @@
 const mainContainer = document.querySelector('.mainContainer');
 const contactContainer = document.querySelector('#contactMainContainer');
 const projectsContainer = document.querySelector('#projectsMainContainer');
+
+const projectsLink = document.querySelector('#projectsLink');
+const homeLink = document.querySelector('#homeLink');
+const contactLink = document.querySelector('#contactLink');
+
 const projectBtn = document.getElementById('projectBtn');
 const contactBtn = document.getElementById('contactBtn');
 const goBackBtn = document.getElementById('goBackBtn');
+const goBackFromProjectsBtn = document.getElementById('goBackBtnFromProjects');
 
 
-projectBtn.addEventListener('click', () => {
-    mainContainer.classList.add('swipe-left');
+function swipeTo(fromContainer, toContainer, outClass, inClass) {
+    fromContainer.classList.add(outClass);
+    fromContainer.addEventListener("animationend", function handleSwipeOut() {
+        fromContainer.style.display = 'none';
+        fromContainer.classList.remove(outClass);
 
-    mainContainer.addEventListener("animationend", function handleMainSwipeOut (){
-        mainContainer.style.display = 'none';
-        projectsContainer.style.display = 'flex';
-        projectsContainer.classList.add('swipeToProjects');
+        toContainer.style.display = 'flex';
+        toContainer.classList.add(inClass);
 
-        mainContainer.classList.remove('swipe-left');
-        mainContainer.removeEventListener('animationend', handleMainSwipeOut);
-    })
-});
-
-contactBtn.addEventListener('click', () => {
-    mainContainer.classList.add('swipe-right');
-
-    mainContainer.addEventListener('animationend', function handleMainSwipeOut() {
-        mainContainer.style.display = 'none';
-        contactContainer.style.display = 'flex';
-        contactContainer.classList.add('swipe-in');
-
-
-        mainContainer.classList.remove('swipe-right');
-        mainContainer.removeEventListener('animationend', handleMainSwipeOut);
-    });
-});
-
-goBackBtn.addEventListener('click', () => {
-    contactContainer.classList.add('swipe-out');
-
-    contactContainer.addEventListener('animationend', function handleContactSwipeOut() {
-        contactContainer.style.display = 'none';
-        contactContainer.classList.remove('swipe-in', 'swipe-out');
-
-        mainContainer.style.display = 'flex';
-        mainContainer.classList.add('swipeBackToMainPage');
-
-        mainContainer.addEventListener('animationend', function handleMainSwipeBack() {
-            mainContainer.classList.remove('swipeBackToMainPage');
-            mainContainer.removeEventListener('animationend', handleMainSwipeBack);
+        // Clean up incoming animation
+        toContainer.addEventListener("animationend", function handleSwipeIn() {
+            toContainer.classList.remove(inClass);
+            toContainer.removeEventListener("animationend", handleSwipeIn);
         });
 
-        contactContainer.removeEventListener('animationend', handleContactSwipeOut);
+        fromContainer.removeEventListener("animationend", handleSwipeOut);
     });
+}
+
+// Gå til projekter
+projectBtn.addEventListener('click', () => {
+    swipeTo(mainContainer, projectsContainer, 'swipe-left', 'swipe-to-projects');
+    homeLink.style.borderBottom = 'none';
+    projectsLink.style.borderBottom = '1px solid black';
+    projectsLink.style.pointerEvents = 'none';
+    homeLink.style.pointerEvents = 'auto';
 });
 
+// Gå til kontakt
+contactBtn.addEventListener('click', () => {
+    swipeTo(mainContainer, contactContainer, 'swipe-right', 'swipe-in');
+    homeLink.style.borderBottom = 'none';
+    contactLink.style.borderBottom = '1px solid black';
+    contactLink.style.pointerEvents = 'none';
+    homeLink.style.pointerEvents = 'auto';
+});
 
+// Gå tilbage fra kontakt
+goBackBtn.addEventListener('click', () => {
+    swipeTo(contactContainer, mainContainer, 'swipe-out', 'swipe-back-to-main');
+    contactLink.style.borderBottom = 'none';
+    contactLink.style.pointerEvents = 'auto';
+    homeLink.style.borderBottom = '1px solid black';
+    homeLink.style.pointerEvents = 'none';
+});
 
+// Gå tilbage fra projekter
+goBackFromProjectsBtn.addEventListener('click', () => {
+    swipeTo(projectsContainer, mainContainer, 'swipe-right', 'swipe-from-left-to-right');
+    projectsLink.style.borderBottom = 'none';
+    projectsLink.style.pointerEvents = 'auto';
+    homeLink.style.borderBottom = '1px solid black';
+    homeLink.style.pointerEvents = 'none';
+});
+
+// Gå til projekter
+projectsLink.addEventListener('click', () => {
+    swipeTo(mainContainer, projectsContainer, 'swipe-left', 'swipe-to-projects');
+    homeLink.style.borderBottom = 'none';
+    projectsLink.style.borderBottom = '1px solid black';
+    projectsLink.style.pointerEvents = 'none';
+    homeLink.style.pointerEvents = 'auto';
+})
+
+// Gå til kontakter
+contactLink.addEventListener('click', () => {
+    swipeTo(mainContainer, contactContainer, 'swipe-right', 'swipe-in');
+    homeLink.style.borderBottom = 'none';
+    contactLink.style.borderBottom = '1px solid black';
+    contactLink.style.pointerEvents = 'none';
+    homeLink.style.pointerEvents = 'auto';
+})
